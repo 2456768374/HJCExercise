80fb52e81875d5c2083898593d11ca9791ceff92
Kritu <2456768374@qq.com>
2023-10-29 21:37:35 +0800

    项目初始化

---
+++ src/main/java/com/hjc/heu/Main.java
@@ -0,0 +1,12 @@
+package com.hjc.heu;
+
+/**
+ * @Auther: Kritu
+ * @Date: 2023/10/29 21:36
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
